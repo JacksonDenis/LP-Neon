fbb6d5a928526d4a0f0629c42a340a65677e45f9
Jackson Denis <jackson.denis@live.com>
2024-01-11 15:53:38 -0300

    neon finalizido com JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-const btnProdutosNeon = document.querySelector('.menuProdutosNeon');
-const dropdownProdutosNeon = document.querySelector('.dropdown');
+const btnProdutosNeon = document.querySelector(".menuProdutosNeon");
+const dropdownProdutosNeon = document.querySelector(".dropdown");
+const btmMobile = document.querySelector(".btn-mobile");
+const overlay = document.querySelector(".overlay");
+const header = document.querySelector("header");
 
 function openDropdown(event) {
     event.preventDefault();
-    dropdownProdutosNeon.classList.toggle('active')
+    dropdownProdutosNeon.classList.toggle("active");
 }
 
-btnProdutosNeon.addEventListener('click', openDropdown);
-dropdownProdutosNeon.addEventListener('mouseleave', openDropdown);
+function fixedMenu() {
+    if (window.pageYOffset > 80) {
+        header.classList.add("fixed-menu");
+    } else {
+        header.classList.remove("fixed-menu");
+    }
+}
+
+function openMenuMobile() {
+    document.documentElement.classList.toggle('menu-opened')
+    
+}
+
+
+document.addEventListener("scroll", fixedMenu);
+btnProdutosNeon.addEventListener("click", openDropdown);
+dropdownProdutosNeon.addEventListener("mouseleave", openDropdown);
+btmMobile.addEventListener("click",openMenuMobile)
+overlay.addEventListener("click",openMenuMobile)
